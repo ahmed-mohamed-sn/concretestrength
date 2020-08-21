4050b04c853ae396b560c7ebd238d808d8cd1e33
Ahmed Mohamed <hanoush87@gmail.com>
2020-08-21 10:52:36 +0200

    initial commit after creating cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An API based serving example using MLFlow',
+    author='Ahmed Mohamed',
+    license='MIT',
+)
